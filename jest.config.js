167bd178027921053e824a9dd93eaccc560af81a
Pedro Reis <pedroreisv@gmail.com>
2019-09-15 18:25:00 -0300

    Setup jest with jsdom 14

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1"
+  },
+  testEnvironment: "jest-environment-jsdom-fourteen"
 };
