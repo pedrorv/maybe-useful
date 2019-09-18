28d880df8fd8060ed5cc511b8f49b2958643809a
Pedro Reis <pedroreisv@gmail.com>
2019-09-18 17:53:28 -0300

    Rename loggers to trackers + Rename tests

---
+++ __tests__/tracker.spec.ts
@@ -0,0 +1,65 @@
+import mouse from "@/trackers/mouse";
+
+describe("Tracker", () => {
+  let clickSpy;
+
+  beforeEach(() => {
+    document.body.innerHTML = `
+        <div class="outer">
+          <div id="middle" class="middle">
+            <div class="inner super-inner">Inner Text</div>
+          </div>
+        </div>
+      `;
+
+    clickSpy = jest.spyOn(mouse, "click");
+
+    document.addEventListener("click", clickSpy);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should trigger the click tracker", () => {
+    (document.querySelector("div.outer") as HTMLElement).click();
+
+    expect(clickSpy).toHaveBeenCalledTimes(1);
+
+    (document.querySelector("div#middle") as HTMLElement).click();
+
+    expect(clickSpy).toHaveBeenCalledTimes(2);
+
+    (document.querySelector("div.inner") as HTMLElement).click();
+
+    expect(clickSpy).toHaveBeenCalledTimes(3);
+  });
+
+  it("should trigger the click tracker even if the dom gets changed after the event listener is added", () => {
+    document.body.innerHTML = `
+        <div class="outer-new"></div>
+      `;
+
+    (document.querySelector("div.outer-new") as HTMLElement).click();
+
+    expect(clickSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should return the eventPath with ids and classes", () => {
+    document.body.innerHTML = `
+        <div class="outer">
+          <div id="middle" class="middle">
+            <div id="inner super-inner" class="inner super-inner">Inner Text</div>
+          </div>
+        </div>
+      `;
+
+    (document.querySelector("div.inner") as HTMLElement).click();
+
+    const returnValue = clickSpy.mock.results[0].value;
+
+    expect(returnValue.eventPath).toEqual(
+      "html body div.outer div#middle.middle div#inner#super-inner.inner.super-inner"
+    );
+  });
+});
