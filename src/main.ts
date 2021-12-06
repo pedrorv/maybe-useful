5f7509f690813dd8229349d897986e83f1b6e12f
Pedro Reis <pedroreisv@gmail.com>
2021-12-05 21:59:57 -0300

    Start watcher on page load

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import drag from './trackers/drag';
 import keyboard from "./trackers/keyboard";
 import mouse from "./trackers/mouse";
+import { getEvents } from './utils/logger';
 
 export const setupTrackers = () =>
   [drag, keyboard, mouse].forEach(tracker =>
-    Object.keys(tracker).forEach(eventName =>
-      document.addEventListener(eventName, tracker[eventName])
-    )
+    Object.keys(tracker).forEach(eventName => {
+      document.removeEventListener(eventName, tracker[eventName]);
+      document.addEventListener(eventName, tracker[eventName]);
+    })
   );
+
+export { getEvents };
+
+if (window && window.addEventListener) {
+  window.addEventListener("load", setupTrackers);
+}
