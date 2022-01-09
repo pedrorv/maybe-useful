83a9f0e23d3a0cd636c9bfc947e8f727814057e9
Pedro Reis <pedroreisv@gmail.com>
2022-01-09 18:52:56 -0300

    Generate heatmap-data.json

--- scripts/process-data.ts
+++ scripts/process-data.ts
@@ -14,7 +14,28 @@ events.forEach((event) => {
     (eventsByPath[event.eventType][event.eventName][event.path] ?? 0) + 1;
 });
 
+const mouseEventsData = {};
+
+const mouseEvents = events.filter((e) =>
+  ["mouse", "drag"].includes(e.eventType)
+);
+
+mouseEvents.forEach((me) => {
+  const key = `${me.properties.clientX}-${me.properties.clientY}`;
+  mouseEventsData[key] = (mouseEventsData[key] ?? 0) + 1;
+});
+
+const heatmapData = Object.keys(mouseEventsData).map((key) => {
+  const [x, y] = key.split("-");
+  return { x: +x, y: +y, value: mouseEventsData[key] };
+});
+
 fs.writeFileSync(
   path.join(__dirname, "events-by-path.json"),
   JSON.stringify(eventsByPath, null, 2)
+);
+
+fs.writeFileSync(
+  path.join(__dirname, "heatmap-data.json"),
+  JSON.stringify(heatmapData, null, 2)
 );
