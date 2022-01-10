5ef69a95e9426b7ab943c4a0f2c2365a3ddca487
Pedro Reis <pedroreisv@gmail.com>
2022-01-10 08:52:11 -0300

    Send events to server

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,31 @@
 const events: any[] = [];
 
+let sending = false;
+setInterval(async () => {
+  if (sending) return;
+  const eventsToSend = events.splice(0, 20);
+  if (!eventsToSend.length) return;
+
+  try {
+    sending = true;
+    const response = await fetch("http://localhost:3000/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(eventsToSend),
+    }).then((res) => res.json());
+
+    if (response.error) {
+      events.push(...eventsToSend);
+    }
+  } catch (e) {
+    events.push(...eventsToSend);
+  } finally {
+    sending = false;
+  }
+}, 300);
+
 export const logEvent = (e: any): void => {
   events.push(e);
 };
