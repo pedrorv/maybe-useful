59ae22cd929c62393558a8ed3b758debbc1566d7
Pedro Reis <pedroreisv@gmail.com>
2022-04-27 00:01:50 -0300

    Fix font warnings

--- src/utils/screenshot.ts
+++ src/utils/screenshot.ts
@@ -4,6 +4,7 @@ export const takeScreenshot = async (): Promise<string | null> => {
   try {
     const canvas = await toCanvas(document.body, {
       backgroundColor: getComputedStyle(document.body).backgroundColor,
+      fontEmbedCSS: "",
     });
 
     return canvas.toDataURL("image/webp", 0.9);
