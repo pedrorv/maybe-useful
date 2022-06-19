9e554c9f412c927ced59024ee5481e3046d2321a
Pedro Reis <pedroreisv@gmail.com>
2022-06-19 16:53:39 -0300

    Remove package logger

--- src/packages/html-screen-capture-js/index.ts
+++ src/packages/html-screen-capture-js/index.ts
@@ -1,7 +1,11 @@
-import { OutputType, LogLevel, Options, CaptureOutput, CaptureFunction } from './types';
-import { goCapture } from './capturer';
+import { OutputType, Options, CaptureOutput, CaptureFunction } from "./types";
+import { goCapture } from "./capturer";
 
-export { OutputType, LogLevel, Options, CaptureOutput, CaptureFunction } from './types';
-export const capture: CaptureFunction = (outputType?, htmlDocument?, options?) => {
-    return goCapture(outputType, htmlDocument, options);
+export { OutputType, Options, CaptureOutput, CaptureFunction } from "./types";
+export const capture: CaptureFunction = (
+  outputType?,
+  htmlDocument?,
+  options?
+) => {
+  return goCapture(outputType, htmlDocument, options);
 };
--- src/packages/html-screen-capture-js/types.ts
+++ src/packages/html-screen-capture-js/types.ts
@@ -1,30 +1,21 @@
 export enum OutputType {
-    OBJECT = 'object',
-    STRING = 'string',
-}
-export enum LogLevel {
-    DEBUG = 'debug',
-    INFO = 'info',
-    WARN = 'warn',
-    ERROR = 'error',
-    FATAL = 'fatal',
-    OFF = 'off',
+  OBJECT = "object",
+  STRING = "string",
 }
 export interface Options {
-    rulesToAddToDocStyle?: string[];
-    cssSelectorsOfIgnoredElements: string[];
-    tagsOfSkippedElementsForChildTreeCssHandling?: string[];
-    attrKeyForSavingElementOrigClass?: string;
-    attrKeyForSavingElementOrigStyle?: string;
-    prefixForNewGeneratedClasses?: string;
-    prefixForNewGeneratedPseudoClasses?: string;
-    imageFormatForDataUrl?: string;
-    imageQualityForDataUrl?: number;
-    logLevel?: LogLevel;
+  rulesToAddToDocStyle?: string[];
+  cssSelectorsOfIgnoredElements: string[];
+  tagsOfSkippedElementsForChildTreeCssHandling?: string[];
+  attrKeyForSavingElementOrigClass?: string;
+  attrKeyForSavingElementOrigStyle?: string;
+  prefixForNewGeneratedClasses?: string;
+  prefixForNewGeneratedPseudoClasses?: string;
+  imageFormatForDataUrl?: string;
+  imageQualityForDataUrl?: number;
 }
 export type CaptureOutput = HTMLElement | string | null;
 export type CaptureFunction = (
-    outputType?: OutputType,
-    htmlDocument?: HTMLDocument,
-    options?: Options,
+  outputType?: OutputType,
+  htmlDocument?: HTMLDocument,
+  options?: Options
 ) => CaptureOutput;
