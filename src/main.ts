ecb0a39befb153523c934c23aca37e7daa134824
Pedro Reis <pedroreisv@gmail.com>
2022-06-19 20:54:00 -0300

    domObserver -> setInterval

--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,13 @@ import {
 } from "./trackers";
 import { setAppId, setServerUrl } from "./utils/common";
 
+let interval;
 export const init = (appId: string, serverUrl: string) => {
   setAppId(appId);
   setServerUrl(serverUrl);
+  clearInterval(interval);
 
-  let domObserver;
-  if (MutationObserver) {
-    domObserver = new MutationObserver((mutations: MutationRecord[]) => {
-      mutations.forEach((m) => {
-        const hasAlteredVisualElements = !![
-          ...Array.from(m.addedNodes),
-          ...Array.from(m.removedNodes),
-        ].filter((node) => node.nodeName !== "SCRIPT");
-
-        if (hasAlteredVisualElements) UITracker.trackDOMChange();
-      });
-    });
-  }
-
-  domObserver?.disconnect();
-  domObserver.observe(document.documentElement || document.body, {
-    childList: true,
-    subtree: true,
-  });
+  interval = setInterval(UITracker.trackDOMChange, 1000);
 
   [UITracker, DragTracker, KeyboardTracker, MouseTracker].forEach((tracker) =>
     tracker.eventNames.forEach((name) => {
